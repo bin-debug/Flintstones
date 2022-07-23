317723b123fe9dae188cd786964d3715271faf83
bin-debug <nivashsingh@gmail.com>
2022-07-23 18:50:45 +0200

    busy with backoffice

--- FlintstonesBackOffice/Pages/Markets.razor.cs
+++ FlintstonesBackOffice/Pages/Markets.razor.cs
@@ -1,23 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.JSInterop;
-using FlintstonesBackOffice;
-using FlintstonesBackOffice.Shared;
-using FlintstonesBackOffice.Models;
-using MudBlazor;
-using FlintstonesEntities;
 using FlintstonesBackOffice.Services;
-using Azure.Data.Tables;
+using FlintstonesEntities;
+using MudBlazor;
 
 namespace FlintstonesBackOffice.Pages
 {
